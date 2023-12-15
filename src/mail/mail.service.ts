269aab7c7dd77ae27b9073fe992c21d304c982ec
GlebChiz <gchyz@jsdaddy.io>
2023-12-15 09:03:51 +0200

    feat: styles were added for revocation confirmation

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,16 +10,17 @@ export class MailService {
   ) {}
 
   public async sendRevocationConfirmation(): Promise<void> {
-    const url = 'confirmUrl';
-    const revocation = 'I like it';
     return await this.mailerService.sendMail({
       from: this.configService.get('MAIL_FROM'),
       to: this.configService.get('MAIL_TO'),
       subject: 'Welcome! Confirm Revocation',
       template: './revocation-confirmation',
       context: {
-        url,
-        revocation,
+        url: 'confirmUrl',
+        revocation:
+          'Лорем іпсум долор сіт амет, консектетур адіпісцінг еліт. Нуллам ін щам вітає юсто тінцідунт аліквам.',
+        revocationOwner: 'Hlib',
+        rating: 5,
       },
     });
   }
